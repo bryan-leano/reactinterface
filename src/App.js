bebca56a7e6287696eb3a6b8950557f3e56413af
Bryan Leanoe <bryan.leano@gmail.com>
2021-10-20 14:06:45 -0600

    add componenets and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { BiArchive } from 'react-icons/bi';
+import { BiCalendar, BiTrash } from 'react-icons/bi';
+import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
+import appointmentList from './data.json';
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments
+      </h1>
+      <AddAppointment />
+      <Search />
+
+      <ul classaName="divide-y divid-gray-200">
+        {appointmentList.map(appointment => (
+          
+        ))}
+      </ul>
     </div>
   );
 }
